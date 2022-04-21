a8b63ef2dca56ec0d2d5e3f6cafc03e4c790b354
Doai Tran <josdoaitran@gmail.com>
2022-04-21 08:31:24 +0700

    update sql database

--- setups/category_table_data.sql
+++ setups/category_table_data.sql
@@ -1,18 +1,34 @@
-INSERT INTO category (category_id, name, last_update) VALUES ('1','Action','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('2','Animation','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('3','Children','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('4','Classics','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('5','Comedy','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('6','Documentary','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('7','Drama','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('8','Family','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('9','Foreign','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('10','Games','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('11','Horror','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('12','Music','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('13','New','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('14','Sci-Fi','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('15','Sports','2022-03-22 19:10:25-07');
-INSERT INTO category (category_id, name, last_update) VALUES ('16','Travel','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+1','Action','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+2','Animation','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+3','Children','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+4','Classics','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+5','Comedy','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+6','Documentary','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+7','Drama','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+8','Family','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+9','Foreign','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+10','Games','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+11','Horror','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+12','Music','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+13','New','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+14','Sci-Fi','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+15','Sports','2022-03-22 19:10:25-07');
+INSERT INTO category (category_id, name, last_update) VALUES ('
+16','Travel','2022-03-22 19:10:25-07');
 
 

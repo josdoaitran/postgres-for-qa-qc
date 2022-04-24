e7963a53a7174fb2ecefaaf959c31cf7f68d07e2
Doai Tran <josdoaitran@gmail.com>
2022-04-24 08:29:53 +0700

    update payment language data

---
+++ setups/language_table_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO language (language_id, name, last_update) VALUES ('1','English','2022-03-22 19:10:25-07');
+INSERT INTO language (language_id, name, last_update) VALUES ('2','Italian','2022-03-22 19:10:25-07');
+INSERT INTO language (language_id, name, last_update) VALUES ('3','Japanese','2022-03-22 19:10:25-07');
+INSERT INTO language (language_id, name, last_update) VALUES ('4','Mandarin','2022-03-22 19:10:25-07');
+INSERT INTO language (language_id, name, last_update) VALUES ('5','French ','2022-03-22 19:10:25-07');
+INSERT INTO language (language_id, name, last_update) VALUES ('6','German ','2022-03-22 19:10:25-07');
